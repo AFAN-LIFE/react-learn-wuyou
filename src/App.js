e5ce472125914485a92c94f0aebdff747cdbfbfb
afan <bili_afan@163.com>
2025-09-09 10:54:43 +0800

    数据渲染

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import logo from './logo.svg';
+import {Fragment} from 'react';
 import './App.css';
 
 function App() {
+
+  // 方法1：普通插入
+  // const divContent = '标签内容'
+  const divTitle   = '标签标题'
+
+
+  // 方法2：条件插值
+  let divContent = null
+  const flag = true
+  if (flag) {
+    // 之前js需要写成字符串结构
+    // divContent = '<span>flag为true</span>'
+    // 但是jsx可以直接书写
+    divContent = <span>flag为true</span>
+  } else {
+    divContent = <p>flag为true</p>
+  }
+
+  // 方法3：列表插值
+  // 推荐给每个值增加一个id，这应该是后端返回的时候就有对应的id
+  const list = [
+    {'id': 1, 'name': '小明'}, 
+    {'id': 2, 'name': '小华'}, 
+    {'id': 3, 'name': '小李'}
+  ]
+  // map是有返回值的
+  const listContent = list.map(item => (
+    // 更好的方式是在外面增加一个组件结构
+    <Fragment>
+    {/* // 要确保同级元素key唯一，不要直接将数据作为key，不然key也会轻易变 */}
+    <li key={item.id}>{item.name}</li>
+    {/* // 如果有多个根元素，但是jsx只能有一个li，可以在外部加<> </> 空标签 */}
+    <li> -------------- </li>
+    </Fragment>
+  ))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div title={divTitle}>
+        {divContent}
+      </div>
+
+      <ul>{listContent}</ul>
+    </>
   );
 }
 
