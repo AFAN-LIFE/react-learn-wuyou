157fd413be0ff6c18138aff804c41b8ff5b4a8ac
afan <bili_afan@163.com>
2025-09-11 10:33:17 +0800

    JSX插槽

--- README.md
+++ README.md
@@ -257,6 +257,7 @@ export default App;
 ```
 
 # 组件通信
+
 ## DOM组件
 
 DOM 组件是 React支持的所有HTML和SVG标签，在React称作Props，React使用Props和HTML还是不一样的
@@ -402,3 +403,69 @@ export default function App() {
   );
 }
 ```
+
+## JSX插槽
+将JSX作为Props传递，父组件除了传递DOM和React组件中的普通值外，还可以传递JSX，类似Vue的插槽概念
+- 需求1： 子组件li作为children插入
+- 需求2： 想在前后插入title和footer 实际中还可以将列表抽象成数组
+
+```
+// // 组件标签自动会作为Props的属性，预定义的children就是内部的子元素
+// function List ({children}) {
+//   return (
+//     <>
+//       <h2>
+//           {title}
+//       </h2>
+//       <ul>
+//           {children}
+//       </ul>
+//     </>
+//   )
+// }
+
+// 因为有的不传递footer，所以footer要有一个默认值
+function List ({children, title, footer=<div>默认底部</div>}) {
+  return (
+    <>
+      <h2>
+          {title}
+      </h2>
+      <ul>
+          {children}
+      </ul>
+      {footer}
+    </>
+  )
+}
+
+export default function App() {
+  // 组件通信
+  // 3 将JSX作为Props传递 
+  // 父组件除了传递DOM和React组件中的普通值外，还可以传递JSX，类似Vue的插槽概念
+
+  // 需求1： 子组件li作为children插入
+  // 需求2： 想在前后插入title和footer 实际中还可以将列表抽象成数组
+  
+  return (
+    <>
+      <List
+        title="列表1"
+        footer={<p>这是底部内容</p>}
+      > 
+        <li>列表项1</li>
+        <li>列表项1</li>
+        <li>列表项1</li>
+      </List>
+      <List> 
+        <li>列表项2</li>
+        <li>列表项2</li>
+        <li>列表项2</li>
+      </List>
+    </>
+  );
+}
+```
+
+## 子组件传父组件
+注意：父组件传递给子组件的props都是单向（只读的），不要尝试修改
--- src/App.js
+++ src/App.js
@@ -1,85 +1,55 @@
-// 用一个props写
-// function Article (props) {
+// // 组件标签自动会作为Props的属性，预定义的children就是内部的子元素
+// function List ({children}) {
 //   return (
-//     <div>
-//         <h2> {props.title} </h2>
-//         <p> {props.content} </p>
-//     </div>
+//     <>
+//       <h2>
+//           {title}
+//       </h2>
+//       <ul>
+//           {children}
+//       </ul>
+//     </>
 //   )
 // }
 
-// 也可以输入的时候就展开
-// function Article ({title, content, active}) {
-//   return (
-//     <div>
-//         <h2> {title} </h2>
-//         <p> {content} </p>
-//         <p> 状态：{active ? '显示' : '隐藏'} </p>
-//     </div>
-//   )
-// }
-
-// 还可以继续分层
-// 更多细节不需要在Article做
-function Detail ({content, active}) {
+// 因为有的不传递footer，所以footer要有一个默认值
+function List ({children, title, footer=<div>默认底部</div>}) {
   return (
     <>
-      <p> {content} </p>
-      <p> 状态：{active ? '显示' : '隐藏'} </p>
+      <h2>
+          {title}
+      </h2>
+      <ul>
+          {children}
+      </ul>
+      {footer}
     </>
-  )
-}
-
-function Article ({title, detailData}) {
-  return (
-    <div>
-        <h2> {title} </h2>
-        <Detail {...detailData}/>
-    </div>
   )
 }
 
 export default function App() {
   // 组件通信
-  // 2 React组件的Props 自定义处理组件处理
+  // 3 将JSX作为Props传递 
+  // 父组件除了传递DOM和React组件中的普通值外，还可以传递JSX，类似Vue的插槽概念
 
-  // 需求1：希望对组件的一些内容进行定制
-  // Step1 请求功能所需的数据，比如文章的信息
-  // Step2 创建Article 组件 并将文章数据分别传递给 Article
-
-  // 需求2：在React组件用展开操作
-  // 实际会有更多复杂的属性，需要将标题和内容区域做一个封装
-
-  // 实际对于细节属性可能在接口返回的时候就会做一个包裹
-  const articleData = {
-    title: '标题1', 
-    detailData: {
-        content: "内容1",
-        active: true
-    }
-  }
-
+  // 需求1： 子组件li作为children插入
+  // 需求2： 想在前后插入title和footer 实际中还可以将列表抽象成数组
+  
   return (
     <>
-      {/* <Article
-        title="标签1"
-        content="内容1" 
-        active         // 特别像DOM中的selected 或者autoplay 属性，不需要给值，默认是true
-      />
-      <Article
-        title="标签2"
-        content="内容2" 
-        active
-      />
-      <Article
-        title="标签3"
-        content="内容3" 
-        active
-      /> */}
-
-      <Article
-        {...articleData}
-      />
+      <List
+        title="列表1"
+        footer={<p>这是底部内容</p>}
+      > 
+        <li>列表项1</li>
+        <li>列表项1</li>
+        <li>列表项1</li>
+      </List>
+      <List> 
+        <li>列表项2</li>
+        <li>列表项2</li>
+        <li>列表项2</li>
+      </List>
     </>
   );
 }
